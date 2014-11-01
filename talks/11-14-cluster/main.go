0ad3b26933e5bc42113b40565cc1469a8634b8d2
Ross Hendrickson <ross.hendrickson@webfilings.com>
2014-11-01 01:01:05 -0500

    Add into main simple http routing and handling

--- talks/11-14-cluster/main.go
+++ talks/11-14-cluster/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 
+	http.HandleFunc("/predict/", classifyUser)
+	panic(http.ListenAndServe(":8080", nil))
+}
+
+func classifyUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
